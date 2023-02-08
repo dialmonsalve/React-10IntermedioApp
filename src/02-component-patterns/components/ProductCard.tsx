d7c43e02bd6827b579dc422633f71aa91ccc5ab9
Diego Alejandro <diales1981@gmail.com>
2023-02-08 14:26:32 -0500

    Control props implementado

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -3,7 +3,7 @@ import { useProduct } from '../hooks/useProduct';
 
 import styles from '../styles/styles.module.css';
 
-import { Product, ProductContextProps } from '../interfaces/Interfaces';
+import { onChangeArgs, Product, ProductContextProps } from '../interfaces/Interfaces';
 
 export const ProductContext = createContext({} as ProductContextProps)
 const { Provider } = ProductContext;
@@ -13,11 +13,13 @@ export interface Props {
 	product: Product;
 	className?: string;
 	style?: React.CSSProperties;
+	onChange?: (args:onChangeArgs) => void;
+	value?:number;
 }
 
-export const ProductCard = ({ children, product, className, style }: Props) => {
+export const ProductCard = ({ children, product, className, style, onChange, value }: Props) => {
 
-	const { counter, increaseBy } = useProduct()
+	const { counter, increaseBy } = useProduct( { onChange, product, value } )
 
 	return (
 
